60c206ab0e29d4f085b17827deea68f7eeef8633
Tanner Smith <tanner@tssoftware.net>
2013-04-07 14:15:11 -0400

    Create a Packet.

---
+++ packet.go
@@ -0,0 +1,19 @@
+package arq
+
+import "fmt"
+
+type Packet struct {
+  SequenceNumber int32
+  ACK bool
+}
+
+// String returns a human-readable string of the Packet
+func (p Packet) String() string {
+  string := fmt.Sprintf("#%d", p.SequenceNumber)
+
+  if p.ACK {
+    string += " - ACK"
+  }
+
+  return string
+}
