59c2b0c4d8bfc130a151deeac70d7865a93d7272
Tanner Smith <tanner@tssoftware.net>
2013-04-07 17:04:20 -0400

    Add tests for queue.go

---
+++ src/arq/queue_test.go
@@ -0,0 +1,107 @@
+package arq
+
+import (
+  "testing"
+)
+
+func TestNewQueue(t *testing.T) {
+  windowSize := 8
+
+  maxSequenceNumber := 2 * windowSize - 1
+
+  queue := NewQueue(windowSize)
+
+  if queue.windowSize != windowSize {
+    t.Errorf("Queue does not have correct window size: %d expected %d", queue.windowSize, windowSize)
+  }
+
+  if queue.baseIndex == 0 && queue.nextSequenceNumberIndex != 0 {
+    t.Error("Queue does not start with an empty window")
+  }
+
+  for _, sequenceNumber := range queue.contents {
+    if sequenceNumber.Sent == true {
+      t.Error("Sent for a sequence number is true, expected false")
+    }
+
+    if sequenceNumber.Acknowledged == true {
+      t.Error("Acknowledged for a sequence number is true, expected false")
+    }
+
+    if sequenceNumber.SequenceNumber > maxSequenceNumber {
+      t.Errorf("Sequence number is %d, expected something less than %d", sequenceNumber.SequenceNumber, maxSequenceNumber)
+    }
+  }
+}
+
+func TestOldestUnacknowledgedSequenceNumber(t *testing.T) {
+  queue := NewQueue(8)
+
+  sequenceNumber, error := queue.OldestUnacknowledgedSequenceNumber()
+
+  if error == nil {
+    t.Error("Error not thrown when no sequence numbers have been sent.")
+  }
+
+  queue.Send()
+
+  sequenceNumber, error = queue.OldestUnacknowledgedSequenceNumber()
+
+  if sequenceNumber.SequenceNumber != 0 {
+    t.Errorf("Oldest sequence number is %d, expected %d", sequenceNumber.SequenceNumber, 0)
+  }
+}
+
+func TestSend(t *testing.T) {
+  queue := NewQueue(8)
+
+  for i := 0; i < queue.windowSize; i++ {
+    sequenceNumber, error := queue.Send()
+
+    if error != nil {
+      t.Errorf("Send returned error on non-full window: %s", error)
+    }
+
+    if sequenceNumber != i {
+      t.Errorf("Send did not return correct sequence number (%d), expected %d", sequenceNumber, i)
+    }
+  }
+
+  _, error := queue.Send()
+
+  if error == nil {
+    t.Error("Send failed to error on full window")
+  }
+}
+
+func TestMarkAcknowledged(t *testing.T) {
+  queue := NewQueue(8)
+
+  error := queue.MarkAcknowledged(0)
+
+  if error == nil {
+    t.Error("Marked a sequence number with no sent sequence numbers")
+  }
+
+  queue.Send()
+
+  error = queue.MarkAcknowledged(0)
+
+  if error != nil {
+    t.Errorf("Expected marking a sent packet to go without error: %s", error)
+  }
+
+  for i := queue.baseIndex; i < queue.nextSequenceNumberIndex; i++ {
+    if queue.contents[i].SequenceNumber == 0 {
+      if !queue.contents[i].Acknowledged {
+        t.Error("Acknowledged field on sequence number is not true")
+      }
+    }
+  }
+
+  error = queue.MarkAcknowledged(50)
+
+  if error == nil {
+    t.Error("Marked a seuqnece number outside of window")
+  }
+}
