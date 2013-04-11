f37754fdf6129de9db6b05f501800babb03dec44
Tanner Smith <tanner@tssoftware.net>
2013-04-10 20:24:59 -0400

    Use Computer class in main class.

--- main/main.go
+++ main/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"arq"
 	"arq/sr"
 	"fmt"
+	"time"
 )
 
 func main() {
-	queue := sr.NewQueue(8)
+	senderOut := make(chan arq.Packet)
+	senderIn := make(chan arq.Packet)
 
-	fmt.Printf("%v\n\n", queue)
+	sender := sr.NewComputer(8, senderIn, senderOut)
+	receiver := sr.NewComputer(8, senderOut, senderIn)
 
-	queue.MarkAcknowledged(0)
+	go func() {
+		if sequenceNumber, err := sender.Send(); err != nil {
+			fmt.Println("Error - ", err)
+		} else {
+			fmt.Printf("Sender sent packet with sequence number %d\n", sequenceNumber)
+		}
+	}()
 
-	fmt.Printf("%v\n", queue)
+	go func() {
+		for {
+			packet, err := receiver.Receive()
+
+			if err != nil {
+				fmt.Println("Error - ", err)
+			} else {
+				fmt.Printf("Receiver received: %v\n", packet)
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			packet, err := sender.Receive()
+
+			if err != nil {
+				fmt.Println("Error - ", err)
+			} else {
+				fmt.Printf("Sender received: %v\n", packet)
+			}
+		}
+	}()
+
+	time.Sleep(30 * time.Second)
 }
